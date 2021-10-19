1a1d732f2f7003f77f755dca8d036c99db7029c0
Prakrityrathore <prakrityrathore@gmail.com>
2021-10-19 22:56:06 +0530

    controller, views, routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/about'
-  root  'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  post '/', to: 'contact#mail'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
